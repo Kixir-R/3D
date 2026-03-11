8e415ddd6b0ff97bcae1acba02ebacff5555b584
Kixir-R <r.jair456@gmail.com>
2026-03-11 14:52:33 -0600

    11.03.26
    11.03.26

---
+++ scripts/player.gd
@@ -0,0 +1,62 @@
+extends CharacterBody3D
+# qué tan rápido se mueve el jugador en m/s
+@export var speed = 14
+# aceleración de caida en m/s2
+@export var fall_acceleration = 75
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
+
+var target_velocity = Vector3.ZERO
+func _physics_process(delta: float):
+# creamos una variable local para almacenar la dirección de entrada
+	var direction = Vector3.ZERO
+	# checamos cada entrada y actualizamos la dirección acorde
+	if Input.is_action_pressed("Left"):
+		direction.x-= 1
+	if Input.is_action_pressed("Right"):
+		direction.x += 1
+	if Input.is_action_pressed("Forward"):
+	# en 3D, el suelo se representa con el plano XZ
+		direction.z-= 1
+	# en 3D, el suelo se representa con el plano XZ
+	if Input.is_action_pressed("Backward"):
+		direction.z += 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# configurando la propiedad Basis que afecta la rotación
+		$Pivot.basis = Basis.looking_at(direction)
+	# vector velocidad en suelo
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+# velocidad de caída
+	if not is_on_floor(): # Si está en el aire, caerá hacia el suelo
+		target_velocity.y = target_velocity.y- (fall_acceleration * delta)
+	# Moviendo al jugador
+	velocity = target_velocity
+	# Iterar a través de todas las colisiones por frame
+	for index in range(get_slide_collision_count()):
+# Tomamos una de las colisiones con el caracter
+		var collision = get_slide_collision(index)
+# Si hay colisiones duplicadas con un enemigo en un frame único
+# el enemigo será borrado después de la primera y una segunda llamada
+# a get_collider retornará null, llevando a un apuntador nulo al llamar
+# collision.get_collider().is_in_group("mob")
+# Este bloque previene el procesar colisiones duplicadas
+		if collision.get_collider() == null:
+			continue
+# Si el collider es con un enemigo
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+# verficaremos que lo hemos golpeado desde arriba
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+			# Si fue así, lo aplastaremos y rebotaremos
+				mob.squash()
+				target_velocity.y = bounce_impulse
+# Prevenimos posteriores llamadas duplicadas
+				break
+	move_and_slide()
+
+# Saltar solo si el jugador está en el suelo y se presiona "jump"
+	if is_on_floor() and Input.is_action_just_pressed("Jump"):
+		target_velocity.y = jump_impulse
